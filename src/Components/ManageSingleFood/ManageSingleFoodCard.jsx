816952ad3229eaa80b1fea60c62091dbeac98df1
asifbuet047 <asifbuet047@gmail.com>
2023-11-09 19:36:30 +0600

    WHen user change status of his added food it will not shown in available food section

--- src/Components/ManageSingleFood/ManageSingleFoodCard.jsx
+++ src/Components/ManageSingleFood/ManageSingleFoodCard.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { convertDate } from '../../Utilities/Utilities'
+import { Radio, RadioGroup, Stack } from '@chakra-ui/react';
+import useAxiosSecure from '../../Hooks/useAxiosSecure';
+import { Button } from 'flowbite-react';
+import { toast } from 'react-toastify';
+import { useNavigate } from 'react-router-dom';
 
 function ManageSingleFoodCard({ data }) {
-    console.log(data);
+    const [value, setValue] = useState(true);
+    const instance = useAxiosSecure();
+    const navigate = useNavigate();
+
+
+    const handleStatusUpdate = () => {
+        instance.post(`/foodStatusUpdate/${data.food_id}`, {
+            food_status: value
+        }).then((response) => {
+            if (response.data.modifiedCount) {
+                toast.success(`Successfully Delivered`, {
+                    position: 'bottom-center',
+                    autoClose: 5000,
+                });
+                navigate('/managefoods');
+            }
+        }).catch((error) => {
+            console.log(error);
+        });
+    }
+
     return (
         <div className="card card-compact w-auto bg-base-100 shadow-xl">
             <figure>
@@ -13,6 +38,14 @@ function ManageSingleFoodCard({ data }) {
                 <h2>Requester mail: {data.requested_user_email}</h2>
                 <h2>Request Date: {convertDate(data.request_date)}</h2>
                 <h2>Additional note: {data.note}</h2>
+                <RadioGroup onChange={setValue} value={value}>
+                    <Stack direction='row'>
+                        <Radio value='true'>Available</Radio>
+                        <Radio value='false'>Delivered</Radio>
+                    </Stack>
+                </RadioGroup>
+                <Button onClick={handleStatusUpdate}>Submit Food Status</Button>
+
             </div>
         </div>
     )
